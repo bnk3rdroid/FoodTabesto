40d55d2d7042d184bff06df6ea7a53b7b16a65f7
bnk3r <bnk3r.droid@gmail.com>
2017-09-06 19:13:46 +0200

    Activity + layout + recyclerview (not bound)

---
+++ FoodTabesto/app/src/main/java/yb/foodtabesto/screens/home/HomeActivity.java
@@ -0,0 +1,18 @@
+package yb.foodtabesto.screens.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import yb.foodtabesto.R;
+
+/**
+ * Home Activity. Display a list of Food products.
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
